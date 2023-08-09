9451a930b204bc0f1ec88df636cb21ac3890ca80
Neehamundra <neehamundra77@gmail.com>
2023-08-09 22:23:42 +0530

    arithematic program

---
+++ Basic/arithematic.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=20;
+    printf("The additional of a and b:%d\n",a+b);
+    printf("The sub of a and b:%d\n",a-b);
+    printf("the reminder of a and b:%d\n",a%b);
+    printf("The division of a and b:%d\n",a/b);
+    return 0;
+}
